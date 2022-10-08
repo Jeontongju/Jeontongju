7602fd054f706c6192aff5c664297b8914c15697
jmh0434 <80581335+jmh0434@users.noreply.github.com>
2022-10-09 00:31:46 +0900

    이미지로 처리한 메인페이지
    이미지로 처리한 메인페이지

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,28 +1,46 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import "../Routes/Home.module.css";
 
-function SectionImg(props){
-    
-}
 function Home(){
-    const navList = ['img/navImg1.jpeg', 'img/navImg2.jpeg', 'img/navImg3.jpeg']
-    const [index, setIndex] = useState(0);
-    return(
-        <div>
-            <header className="header">
-                <h1>조물주 위에 막걸리</h1>
+    return (
+        <div className="home">
+            <header id="header">
+                <img id="title" src="img/title.png" alt="" />
             </header>
             <nav className="nav">
-                <img src={navList[index]}/>
+                <img id="banner" src="img/banner.png" alt="" />
             </nav>
             <article className="menu">
-                <section className="recommendation">
-                    <div>
-                        <Link to={`/drinks/recommendation`}/>
-                    </div>
+                <section>
+                    <table>
+                        <tr>
+                            <td>
+                            <a href="/drinks/recommendation">
+                                <img
+                                id="recommendation"
+                                src="img/recommendation.png"
+                                alt=""
+                                />
+                            </a>
+                            </td>
+                            <td>
+                                <a href="/stores">
+                                    <img id="stores" src="img/stores.png" alt="" />
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
                 </section>
             </article>
+            <article className="list">
+                <header id="listheader">
+                    <img id="drink"src="img/list.png" alt="" />
+                </header>
+                <div id="driklist">
+                    <img src="img/drinklist.png" alt="" />
+                </div>
+            </article>
         </div>
-    )
+    );
 }
 export default Home;
